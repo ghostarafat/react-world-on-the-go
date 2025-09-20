c48efaf76a7ef7b4db8d76db07b83d85b6bd7161
Arafat Hossain <arafathossain173737@gmail.com>
2025-09-21 01:22:13 +0600

    get coutry info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Suspense } from "react";
 import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+)
+.then((res) => res.json());
+
+
+
+
 
 function App() {
   return (
     <>
-      <Countries> </Countries>
+     <Suspense fallback={<p>Loading...</p>}>
+       <Countries countriesPromise={countriesPromise} />
+     </Suspense>
     </>
   );
 }
