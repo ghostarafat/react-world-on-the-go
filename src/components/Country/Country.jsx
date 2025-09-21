26fe7cbc6dadf6f846c6fcab6225ea1d9db20fb3
Arafat Hossain <arafathossain173737@gmail.com>
2025-09-22 04:06:50 +0600

    flag card added, total country visited

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Country.css";
+
+const Country = ({ country, handleVisitedCountries }) => {
+  const [visited, setVisited] = useState(false);
+  // console.log(country);
+  console.log(handleVisitedCountries);
+
+  const handleVisited = () => {
+    // basic system
+    // if (visited){
+    //   setVisited(false);
+    // }
+    // else{
+    //   setVisited(true);
+    // }
+
+    // ternary system
+    // setVisited(visited ? false : true);
+
+    // third system
+    setVisited(!visited);
+    handleVisitedCountries(country);
+  };
+
+  return (
+    <div className={`country ${visited ? "country-visited" : ""}`}>
+      <img src={country?.flags?.flags?.png} alt={country.flags.flags.png.alt} />
+      <h2>Name: {country.name.common}</h2>
+      <h3>Capital: {country.capital.capital} </h3>
+      <p>Population: {country.population.population}</p>
+      {/* <p>Language: {country.languages.languages} </p> */}
+      <p>Region:{country.region.region} </p>
+      <p>
+        Area: {country.area.area}{" "}
+        {country.area.area > 300000 ? "Big Country" : "Small Country"}{" "}
+      </p>
+      {/* <p>Currency: {country.currencies.currencies}</p> */}
+      <p>Continent: {country.continents.continents}</p>
+      <button onClick={handleVisited}>
+        {visited ? "visited" : "Not Visited "}
+      </button>
+    </div>
+  );
+};
+
+export default Country;
